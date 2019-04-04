cf4ea9435b6884b1422f6792ccbe5eb0345dd12f
Tom Roussel <tom.roussel@esat.kuleuven.be>
2019-04-04 10:04:02 +0200

    Added setup script

---
+++ setup_vim.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [[ ! -d ~/.vim ]]; then
+	echo "Could not find ~/.vim, did you forget to rename the repo after cloning it?"
+	echo "Quitting"
+	exit 1
+fi
+
+if [[ -f ~/.vimrc ]]; then
+	echo "~/.vimrc already exists! Please remove this manually"
+	exit 1
+fi
+
+echo "source ~/.vim/custom_rc" >> ~/.vimrc
+
+cd ~/.vim
+
+git submodule update --init
+
