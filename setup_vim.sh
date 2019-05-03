b5f5ec1181942cf13812172767da4667568827fe
Tom Roussel <tom.roussel@esat.kuleuven.be>
2019-05-03 09:58:54 +0200

    Minor improvement to install script

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+function check_bin() {
+    if type $1 2>&1 >/dev/null ; then
+        echo "$1 is installed"
+    else
+        echo "WARNING: $1 should be installed, some stuff might not work"
+    fi
+}
+
 if [[ ! -d ~/.vim ]]; then
 	echo "Could not find ~/.vim, did you forget to rename the repo after cloning it?"
 	echo "Quitting"
 	exit 1
 fi
+
+echo "Checking some dependencies..."
+check_bin ack
+check_bin git
 
 if [[ -f ~/.vimrc ]]; then
 	echo "~/.vimrc already exists! Please remove this manually"
